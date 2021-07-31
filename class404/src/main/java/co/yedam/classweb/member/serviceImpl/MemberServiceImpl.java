83644383256f53da0278712852f805e761ce075a
NaeunLeee <nue.an.2@gmail.com>
2021-07-31 16:12:17 +0900

    memberserviceimpl

--- class404/src/main/java/co/yedam/classweb/member/serviceImpl/MemberServiceImpl.java
+++ class404/src/main/java/co/yedam/classweb/member/serviceImpl/MemberServiceImpl.java
@@ -4,37 +4,90 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import co.yedam.classweb.common.DAO;
+import co.yedam.classweb.common.DataSource;
 import co.yedam.classweb.member.service.MemberService;
 import co.yedam.classweb.member.vo.MemberVO;
 
 public class MemberServiceImpl implements MemberService {
-
+	private DataSource dataSource = DataSource.getInstance();
 	private Connection conn;
 	private PreparedStatement psmt;
 	private ResultSet rs;
 	
-	@Override
+	
+	// 회원 목록 조회
 	public List<MemberVO> memberSelectList() {
-		// TODO Auto-generated method stub
-		return null;
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		MemberVO vo;
+		String sql = "select * from member";
+		
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			
+			while (rs.next()) {
+				vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPassword(rs.getString("password"));
+				vo.setName(rs.getString("name"));
+				vo.setAge(rs.getInt("age"));
+				vo.setHobby(rs.getString("hobby"));
+				vo.setAuthor(rs.getString("author"));
+				vo.setState(rs.getString("state"));
+				vo.setClId(rs.getInt("clid"));
+				list.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return list;
 	}
 
-	@Override
+	// 회원 한명 조회
 	public MemberVO memberSelect(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from member where id = ?";
+		
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			
+			while (rs.next()) {
+				vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPassword(rs.getString("password"));
+				vo.setName(rs.getString("name"));
+				vo.setAge(rs.getInt("age"));
+				vo.setHobby(rs.getString("hobby"));
+				vo.setAuthor(rs.getString("author"));
+				vo.setState(rs.getString("state"));
+				vo.setClId(rs.getInt("clid"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return vo;
 	}
 
-	@Override
+	// 로그인
 	public MemberVO memberLogin(MemberVO vo) {
 		
 		String sql = "select name, author from member where id = ? and password = ? and state = 'Y'";
 		
 		try {
-			conn = DAO.getConnection();
+			conn = dataSource.getConnection();
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, vo.getId());
 			psmt.setString(2, vo.getPassword());
@@ -49,28 +102,61 @@ public class MemberServiceImpl implements MemberService {
 			close();
 		}
 		
-		
 		return vo;
 	}
 
-	@Override
+	// 회원가입
 	public int memberInsert(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "insert into member values (?, ?, ?, ?, ?, ?, ?)";
+		int r = 0;
+		
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, vo.getId());
+			psmt.setString(2, vo.getPassword());
+			psmt.setString(3, vo.getName());
+			psmt.setInt(4, vo.getAge());
+			psmt.setString(5, vo.getHobby());
+			psmt.setString(6, vo.getAuthor());
+			psmt.setString(7, vo.getState());
+			r = psmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return r;
 	}
 
-	@Override
+	// 회원 삭제
 	public int memberDelete(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String sql = "delete from member where id = ?";
+		int r = 0;
+		
+		try {
+			conn = dataSource.getConnection();
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, vo.getId());
+			r = psmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return r;
 	}
 
-	@Override
+	// 회원 정보 수정
 	public int memberUpdate(MemberVO vo) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	// 커넥션 종료
 	private void close() {
 		try {
 			if (rs != null) rs.close();
