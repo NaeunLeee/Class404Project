4b9cc8290f75e1668fef263acab69b53ba429e7d
수민 <rnjstnals120@naver.com>
2021-08-01 17:49:49 +0900

    mypage

--- class404/src/main/java/co/yedam/classweb/command/MyPageUpdate.java
+++ class404/src/main/java/co/yedam/classweb/command/MyPageUpdate.java
@@ -4,13 +4,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.yedam.classweb.common.Command;
+import co.yedam.classweb.member.service.MemberService;
+import co.yedam.classweb.member.serviceImpl.MemberServiceImpl;
+import co.yedam.classweb.member.vo.MemberVO;
 
 public class MyPageUpdate implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO 마이페이지 수정업데이트 
-		return null;
+		MemberService dao = new MemberServiceImpl();
+		MemberVO vo = new MemberVO();
+		String page = "";
+		
+		vo.setId(request.getParameter("id"));
+		vo.setPassword(request.getParameter("password"));
+		vo.setName(request.getParameter("name"));
+		vo.setAge(Integer.valueOf(request.getParameter("age")));
+		int n = dao.memberUpdate(vo);
+		
+		if(n != 0) {
+			page = "home/myPage";
+		}else {
+			page = "home/myPageError";
+		}
+		
+		return page;
 	}
 
 }
