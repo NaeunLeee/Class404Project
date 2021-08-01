1c3757349e5a1bf1361ccee7f8489694a98c10be
수민 <rnjstnals120@naver.com>
2021-08-01 21:31:19 +0900

    공지사항 완료

--- class404/src/main/java/co/yedam/classweb/command/InsertBoard.java
+++ class404/src/main/java/co/yedam/classweb/command/InsertBoard.java
@@ -1,16 +1,35 @@
 package co.yedam.classweb.command;
 
+import java.sql.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import co.yedam.classweb.board.service.BoardService;
+import co.yedam.classweb.board.serviceImpl.BoardServiceImpl;
+import co.yedam.classweb.board.vo.BoardVO;
 import co.yedam.classweb.common.Command;
 
 public class InsertBoard implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return "home/boardList";
+		// TODO 게시글 등록
+				BoardService dao = new BoardServiceImpl();
+				BoardVO vo = new BoardVO();
+				vo.setbWriter(request.getParameter("bWriter"));
+				vo.setbTitle(request.getParameter("bTitle"));
+				vo.setbContent(request.getParameter("bContent"));
+				vo.setbDate(Date.valueOf(request.getParameter("bDate")));
+				int n = dao.boardInsert(vo);
+				String page="";
+				if (n != 0) {
+					page="boardList.do";
+				}else {
+					page="home/myPageError";
+				}
+				
+				return page;
 	}
 
 }
